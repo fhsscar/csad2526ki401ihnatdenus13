bfaf092894373e0b4e41ad823677a36c7dd18ccc
LAPTOP-M19B2MQO\Acer <ignatdenus@gmail.com>
2025-10-20 17:18:03 +0300

    Add GoogleTest-based unit tests for math_operations

---
+++ math_operations.h
@@ -0,0 +1,6 @@
+// Create a simple C++ header declaring a function int add(int a, int b);
+
+#pragma once
+
+// Declares a simple addition function
+int add(int a, int b);
---
+++ tests/unit_tests.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include "../math_operations.cpp"
+
+TEST(MathOperationsTest, AddTest) {
+    EXPECT_EQ(add(2, 3), 5);
+    EXPECT_EQ(add(-1, 1), 0);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
